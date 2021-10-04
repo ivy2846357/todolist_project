3c41a2395f97a1211e366a1c9d0725456658f06c
ZAII <ivy2846357@icloud.com>
2021-10-04 19:01:33 +0800

    Update 2021-10-04T11:01:29.996Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -13,21 +13,24 @@ var finishedList = document.querySelector('.list__progress--finished'); //待辦
 
 var unfinishedNum = document.querySelector('.unfinished-num'); //新增事項按鈕
 
-var addBtn = document.querySelector('.list__add-btn'); //移除事項按鈕
+var addBtn = document.querySelector('.list__add-btn'); //移除已完成事項
 
-var deleteBtn = document.querySelector('.list__delete-btn');
+var clearFinishedBtn = document.querySelector('.list__clear-finished');
 var data = [{
-  content: '把冰箱發霉的檸檬拿去丟'
+  content: '把冰箱發霉的檸檬拿去丟',
+  checked: false
 }, {
-  content: '打電話叫媽媽匯款給我'
+  content: '打電話叫媽媽匯款給我',
+  checked: false
 }, {
-  content: '整理電腦資料夾'
+  content: '整理電腦資料夾',
+  checked: false
 }];
 
 function renderData() {
   var listContent = '';
   data.forEach(function (item, index) {
-    listContent += "<li class=\"list__item\">\n        <label class=\"list__content\">\n          <input type=\"checkbox\" class=\"list__checkbox\">\n          <span class=\"list__word\">".concat(item.content, "</span>\n        </label>\n        <a href=\"#\" class=\"list__delete-btn\" data-num=\"").concat(index, "\">\n          <i class=\"fas fa-times\"></i>\n        </a>\n      </li>");
+    listContent += "<li class=\"list__item\">\n        <label class=\"list__content\">\n          <input type=\"checkbox\" class=\"list__checkbox\">\n          <span class=\"list__word\">".concat(item.content, "</span>\n        </label>\n        <a href=\"#\" class=\"list__delete-btn\">\n          <i class=\"fas fa-times\" data-num=\"").concat(index, "\"></i>\n        </a>\n      </li>");
   });
   list.innerHTML = listContent;
   unfinishedNum.textContent = data.length;
@@ -35,7 +38,7 @@ function renderData() {
 
 
 renderData();
-console.log(deleteBtn); //新增待辦事項
+console.log(data); //新增待辦事項
 
 addBtn.addEventListener('click', function (e) {
   if (inputContent.value.trim() == '') {
@@ -47,5 +50,20 @@ addBtn.addEventListener('click', function (e) {
   data.push(listObj);
   renderData();
   inputContent.value = '';
+});
+list.addEventListener('click', function (e) {
+  // let checkboxList = document.querySelectorAll('li');
+  // if (e.target.nodeName == 'SPAN') {
+  //   console.log('11111');
+  // } else {
+  //   return;
+  // }
+  if (e.target.nodeName == 'I') {
+    var num = e.target.getAttribute('data-num');
+    data.splice(num, 1);
+    renderData();
+  } else {
+    return;
+  }
 });
 //# sourceMappingURL=all.js.map
